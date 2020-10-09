1ee2a993027c227b06cd99566acbf8d8d74b7b38
qqpwn <31859931+qqpwn@users.noreply.github.com>
2020-10-09 10:56:50 +0200

    done-0?

---
+++ FanOutLibary/FanOutLibary/FanOutPut.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace FanOutLibary
+{
+    public class FanOutPut
+    {
+        private double _temp;
+        private double _fugt;
+        private string _name;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+
+                if (value.Length < 2) throw new ArgumentException($"Name must be atleast 2 Charactes long");
+                else
+                {
+                    _name = value;
+                }
+
+            }
+        }
+
+        public double Temp
+        {
+            get { return _temp; }
+            set
+            {
+                if (value < 15)
+                {
+                    throw new ArgumentException("temperaturen er for lav, skal være over 15 grader");
+                }
+                else if (value > 25)
+                {
+                    throw new ArgumentException("temperaturen er for høj, skal være under 25 grader");
+                }
+                else
+                {
+                    _temp = value;
+                }
+
+            }
+        }
+
+        public double Fugt
+        {
+            get { return _fugt; }
+            set
+            {
+                if (value < 30)
+                {
+                    throw new ArgumentException("fugten er for lav");
+                }
+                else if (value > 80)
+                {
+                    throw new ArgumentException("fugten er for høj");
+                }
+                else
+                {
+                    _fugt = value;
+                }
+
+
+            }
+        }
+    }
+}
---
+++ FanOutLibary/Opgave6/Server.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using FanOutLibary;
+using Newtonsoft.Json;
+
+
+namespace Opgave6
+{
+    public class Server
+    {
+        public static void Start()
+        {
+            try
+            {
+                TcpListener server = null;
+
+                
+                int port = 4646;
+
+
+                server = new TcpListener(IPAddress.Loopback, port);
+
+                server.Start();
+
+                Console.WriteLine("Waiting for a connection........");
+
+
+
+
+                while (true)
+                {
+
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    Task.Run(() =>
+                    {
+                        TcpClient tempSocket = client;
+                        DoClient(tempSocket);
+                    });
+
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+
+        public static void DoClient(TcpClient socket)
+        {
+
+            List<FanOutPut> fanlist = new List<FanOutPut>() { new FanOutPut() { Id = 1 }, new FanOutPut() { Id = 2 } };
+
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            sw.WriteLine("You are connected!!!");
+
+
+            while (true)
+            {
+
+
+
+                string message = sr.ReadLine();
+
+                if (message.ToLower().Contains("Close"))
+                {
+                    break;
+                }
+
+                switch (message.ToLower())
+                {
+                    case "GetAll":
+
+                        string data = JsonConvert.SerializeObject(fanlist);
+                        sw.WriteLine(data);
+
+                        break;
+
+                    case "Get":
+                        string meassage2 = sr.ReadLine();
+
+                        int idet = Convert.ToInt32(meassage2);
+
+                        FanOutPut fan = fanlist.Find(f => f.Id == idet);
+                        string data2 = JsonConvert.SerializeObject(fan);
+
+                        sw.WriteLine(data2);
+                        break;
+
+                    case "Save":
+
+                        string fanstring = sr.ReadLine();
+                        FanOutPut fan2 = JsonConvert.DeserializeObject<FanOutPut>(fanstring);
+                        fanlist.Add(fan2);
+
+                        break;
+
+                    default:
+                        sw.Write("Please select method");
+                        break;
+
+                }
+
+
+
+
+            }
+            sw.WriteLine("Close");
+
+            ns.Close();
+
+            Console.WriteLine("Stream closed");
+
+            socket.Close();
+            Console.WriteLine("Client closed");
+        }
+    }
+} 
+    
